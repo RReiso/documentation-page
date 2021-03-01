80c76d7ed0c2447e1060ed59473ef7fd6b16bba2
RReiso <ruta.littlemore@gmail.com>
2021-02-28 22:41:15 -0500

    FCC version finished

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
-const toggleMenuBtn = document.querySelector("#open-nav-btn");
-const toggleMenuBtn2 = document.querySelector("#close-nav-btn");
+const OpenMenuBtn = document.querySelector("#open-nav-btn");
+const CloseMenuBtn = document.querySelector("#close-nav-btn");
 const sideMenu = document.querySelector(".navigation");
-// let link = document.querySelectorAll(".nav-link");
 
-toggleMenuBtn.addEventListener("click", () => {
+OpenMenuBtn.addEventListener("click", () => {
 	sideMenu.classList.add("menu-show");
-	toggleMenuBtn.classList.remove("show");
-	toggleMenuBtn.classList.add("hide");
-	toggleMenuBtn2.classList.remove("hide");
-	toggleMenuBtn2.classList.add("show");
+	OpenMenuBtn.classList.remove("show");
+	OpenMenuBtn.classList.add("hide");
+	CloseMenuBtn.classList.remove("hide");
+	CloseMenuBtn.classList.add("show");
 });
 
-toggleMenuBtn2.addEventListener("click", () => {
+CloseMenuBtn.addEventListener("click", () => {
 	sideMenu.classList.remove("menu-show");
-	toggleMenuBtn2.classList.remove("show");
-	toggleMenuBtn2.classList.add("hide");
-	toggleMenuBtn.classList.remove("hide");
-	toggleMenuBtn.classList.add("show");
+	CloseMenuBtn.classList.remove("show");
+	CloseMenuBtn.classList.add("hide");
+	OpenMenuBtn.classList.remove("hide");
+	OpenMenuBtn.classList.add("show");
 });
 
 
